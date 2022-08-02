792bbcdf0655e09d602e5851578a02f320c67ec8
sdifiore <difiores@outlook.com>
2022-08-02 16:27:31 -0300

    Add project files.

---
+++ ButtomMaster.cs
@@ -0,0 +1,19 @@
+﻿public class ButtonMaster
+{
+    public event EventHandler<ButtomPressedEventArgs>? ButtomPreessed;
+
+    public void OnButtonPressed(char keyCode)
+    {
+        ButtomPreessed?.Invoke(this, new ButtomPressedEventArgs(keyCode));
+    }
+}
+
+public class ButtomPressedEventArgs
+{
+    public ButtomPressedEventArgs(char keyCode)
+    {
+        KeyCode = keyCode;
+    }
+
+    public char KeyCode { get; }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿var buttomMaster = new ButtonMaster();
+
+buttomMaster.ButtomPreessed += (sender, args) =>
+{
+    Console.WriteLine($"Button {args.KeyCode} pressed");
+};
+
+buttomMaster.ButtomPreessed += (sender, args) =>
+{
+    Console.WriteLine($"Button {args.KeyCode} pressed from differente handler");
+};
+
+Start:
+
+var keyCode = Console.ReadKey(true).KeyChar;
+buttomMaster.OnButtonPressed(keyCode);
+
+goto Start;
